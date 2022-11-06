09f7aba7ccd0b7e49cfe30215747ece39a3a4171
tejasmr <tejasmr@outlook.com>
2022-11-06 20:51:26 +0530

    Removed comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { exec } from 'child_process';
 
-/**
- * Execute simple shell command (async wrapper).
- * @param {String} cmd
- * @return {Object} { stdout: String, stderr: String }
- */
 async function sh(cmd: string) {
 	return new Promise(function (resolve, reject) {
 		exec(cmd, (err, stdout, stderr) => {
@@ -22,17 +15,7 @@ async function sh(cmd: string) {
 
 const EXTENSION_ID: string = 'undefined_publisher.autopr';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "autopr" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('autopr.autopr', () => {
 		let folders: readonly vscode.WorkspaceFolder[] | undefined = vscode.workspace.workspaceFolders;
 		if (!folders) { 
@@ -54,5 +37,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() { }
