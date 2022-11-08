e7c5b88dde2a150e3837e64ef44e89fc936e0ffa
tejasmr <tejasmr@outlook.com>
2022-11-08 22:17:27 +0530

    Added logs and fixed push script path

--- src/extension.ts
+++ src/extension.ts
@@ -17,18 +17,31 @@ const EXTENSION_ID: string = 'tejasmr.autopr';
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('autopr.autopr', () => {
+		console.log("Retrieving the workspace folders");
 		let folders: readonly vscode.WorkspaceFolder[] | undefined = vscode.workspace.workspaceFolders;
+
 		if (!folders) { 
+			console.log("Error: Workspace folders not found");
 			return; 
 		}
+
+		console.log("Retrieving the path of the extension");
 		let path = vscode.extensions.getExtension(EXTENSION_ID)?.extensionPath;
+
+		console.log("Retrieving the default workspace");
 		let workspacePath = folders[0].uri.path;
-		const getCommand = (value: string|undefined) => 'sh ' + path + '/src/push ' + '"' + (workspacePath) + '" "' + (value ?? 'Adding') + '"';
+
+		console.log("Creating the command for the push script");
+		const getCommand = (value: string|undefined) => 'sh ' + path + '/push ' + '"' + (workspacePath) + '" "' + (value ?? 'Adding') + '"';
+
+		console.log("Showing the user the input box for the commit message");
 		vscode.window.showInputBox({
 			"placeHolder": "Adding",
 			"title": "Enter commit message"
 		}).then((value) => {
+			console.log("Successfully retrieved the commit message from the user");
 			sh(getCommand(value)).then((value) => {
+				console.log("Successfully ran the script");
 				console.log(value);
 			});
 		});
